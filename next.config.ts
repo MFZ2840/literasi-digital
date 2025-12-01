1a75e92cfdcd097ce1e32ee5b6537628acf02237
Faza <mfaza2840@gmail.com>
2025-12-01 19:58:11 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   
-  // Konfigurasi yang sudah ada
   allowedDevOrigins: ["http://192.168.169.15:3000"],
   transpilePackages: ['@ckeditor/ckeditor5-react', '@ckeditor/ckeditor5-build-classic'],
 
-  // Headers untuk development (dari saran Claude AI)
   async headers() {
     return [
       {
@@ -19,12 +17,12 @@ const nextConfig: NextConfig = {
             value: 'nosniff',
           },
           {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://portofolio-mfz.vercel.app;",
           },
         ],
       },
